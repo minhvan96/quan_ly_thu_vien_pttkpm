fdb6ac1806b55c22db770010d371f3adc0a2901c
minhvan96 <74522363+minhvan96@users.noreply.github.com>
2022-11-12 13:40:43 +0700

    FEAT - Init

---
+++ src/LibraryManagementSystem.App/LibraryManagementSystem.App.Form.cs
@@ -0,0 +1,10 @@
+namespace LibraryManagementSystem.App
+{
+    public partial class LibraryManagementSystemUI : Form
+    {
+        public LibraryManagementSystemUI()
+        {
+            InitializeComponent();
+        }
+    }
+}
