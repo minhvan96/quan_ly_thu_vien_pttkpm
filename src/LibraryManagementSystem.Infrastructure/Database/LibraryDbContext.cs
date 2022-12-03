de54d5dfcf81d087a564d5c162179c5f57b1fc84
ThanhDo <vthanhdo2000@gmail.com>
2022-12-03 17:31:45 +0700

    remove Reader

--- src/LibraryManagementSystem.Infrastructure/Database/LibraryDbContext.cs
+++ src/LibraryManagementSystem.Infrastructure/Database/LibraryDbContext.cs
@@ -14,7 +14,6 @@ public class LibraryDbContext : DbContext
     public DbSet<LibraryCard> LibraryCards => Set<LibraryCard>();
     public DbSet<LibraryConfiguration> LibraryConfigurations => Set<LibraryConfiguration>();
     public DbSet<Publisher> Publishers => Set<Publisher>();
-    public DbSet<Reader> Readers => Set<Reader>();
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
